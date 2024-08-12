85a05f157636ad7cc14e9a7729afd32e7d681627
Eduardo Maia <edumaia909@gmail.com>
2024-08-12 09:20:30 -0300

    chore: configure files for deploy

--- src/database/connection.mjs
+++ src/database/connection.mjs
@@ -1,3 +1,3 @@
 import { Sequelize } from 'sequelize'
 
-export const sequelize = new Sequelize('mysql://root:root@localhost:3306/node-ecommerce')
+export const sequelize = new Sequelize('postgresql://user_db:xIH580O73hOALpm4QpmgmTJTTDHLSz2v@dpg-cqsvqglumphs73c4namg-a/ecommerce_so6v')
